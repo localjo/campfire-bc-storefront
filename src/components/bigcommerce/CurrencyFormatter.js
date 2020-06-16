8805de7d280fe18b1e3013842a6289073a2379a4
Jo Sprague <josiah.sprague@gmail.com>
2020-06-16 19:23:56 +0200

    Fix currency display

--- src/components/bigcommerce/CurrencyFormatter.js
+++ src/components/bigcommerce/CurrencyFormatter.js
@@ -1,11 +1,12 @@
 const CurrencyFormatter = ({ currency, amount }) => {
-  const languageCode =
-    typeof window !== 'undefined'
-      ? window.navigator.language || 'en-US'
-      : 'en-US';
+  const languageCode = 'en-US';
+  // Prices are returned by API in the store's currency which is USD (for now)
+  // typeof window !== 'undefined'
+  //   ? window.navigator.language || 'en-US'
+  //   : 'en-US';
   const formattedPrice = new Intl.NumberFormat(languageCode, {
     style: 'currency',
-    currency
+    currency,
   }).format(amount);
   return amount && formattedPrice;
 };
